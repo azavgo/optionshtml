f1d2ea5aa1452d991f4c4bc7f1b6840e4c341ba3
Alex. Z <azavgo@yahoo.com>
2022-09-24 20:34:02 +1000

    Added option calculation functionality

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use statrs::distribution::{Normal, ContinuousCDF};
+
+pub fn option_price(s: f64, k: f64, t: f64, sigma: f64, r: f64) -> f64 {
+    let d1 = (1.0 / (sigma * t.sqrt())) * ((s/k).log10() + (r + 0.5 * sigma * sigma) * t);
+    let d2 = d1 - sigma * t.sqrt();
+    let pv = k * (-r*t).exp();
+    //N - standard normal cumulative distribution function
+    let n = Normal::new(0.0, 1.0).unwrap();
+    let n1 = n.cdf(d1);
+    let n2 = n.cdf(d2); 
+    
+    let c = n1 * s - n2 * pv;
+    c
+}
+
+fn main() {
+    
+    let s = 100.0;
+    let k = 95.0; 
+    let t = 0.25; 
+    let sigma = 0.5; 
+    let r = 0.01; 
+    println!("Option price = ${:.2}", option_price(s, k, t, sigma, r));
+    println!("The correct answer is $12.5279");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*; 
+
+    #[test]
+    fn test_01() {
+        assert_eq!(905.0, 905.0);
+    }
+
+
+}
